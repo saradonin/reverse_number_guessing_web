e6755e3705da32dee44292b7fe166fc2d147bccb
saradonin <m.jemieljanczuk@gmail.com>
2023-03-30 17:44:46 +0200

    Basic funcionality in progress

--- number_guessing_reversed.py
+++ number_guessing_reversed.py
@@ -2,13 +2,31 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+"""
+WORK IN PROGRESS
+"""
 
 @app.route("/", methods=['POST', 'GET'])
-def postcode_valid():
+def guess(min=1, max=1000):
     if request.method == "GET":
         return render_template("form.html")
     else:
-        return render_template("form.html")
+        min = int(request.form.get("min"))
+        max = int(request.form.get("max"))
+        guess = (max - min) // 2 + min
+        # guess = int(request.form.get("guess", 500))
+        answer = request.form.get("answer")
+
+        if answer == "too big":
+            max = guess
+        elif answer == "too small":
+            min = guess
+        elif answer == "you win":
+            return render_template("form.html", result="I won!")
+
+        return render_template("form.html", guess=guess, min=min, max=max)
+
+
 
 
 if __name__ == "__main__":
