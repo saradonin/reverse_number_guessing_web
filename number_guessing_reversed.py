775a46c3ab92f30d2b2d088de4fafe3eb2498f21
saradonin <m.jemieljanczuk@gmail.com>
2023-04-01 10:24:01 +0200

    HTML template split into 3 files. Variables corrected.

--- number_guessing_reversed.py
+++ number_guessing_reversed.py
@@ -7,22 +7,24 @@ WORK IN PROGRESS
 """
 
 @app.route("/", methods=['POST', 'GET'])
-def guess(min=1, max=1000):
+def guess():
     if request.method == "GET":
-        return render_template("form.html")
+        return render_template("start.html")
     else:
         min = int(request.form.get("min"))
         max = int(request.form.get("max"))
-        guess = (max - min) // 2 + min
-        # guess = int(request.form.get("guess", 500))
         answer = request.form.get("answer")
+        # guess = (max - min) // 2 + min
+        guess = int(request.form.get("guess", 500))
 
-        if answer == "too big":
+        if answer == "Too big":
             max = guess
-        elif answer == "too small":
+        elif answer == "Too small":
             min = guess
-        elif answer == "you win":
-            return render_template("form.html", result="I won!")
+        elif answer == "You win!":
+            return render_template("win.html", guess=guess)
+
+        guess = (max - min) // 2 + min
 
         return render_template("form.html", guess=guess, min=min, max=max)
 
@@ -30,7 +32,7 @@ def guess(min=1, max=1000):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
 
-# curl -X GET localhost:5000/
-# curl -X POST localhost:5000/
+# curl -X GET localhost:5001/
+# curl -X POST localhost:5001/
